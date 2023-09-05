32738d8bc0a03614c880650945c5b137e35c3f47
Vladislav Lanskikh <lanvlad911@gmail.com>
2023-09-05 17:26:35 +0200

    Add aes encryption/decription demo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,45 @@
-console.log("Hello, world!");
+import AES from "crypto-js/aes";
+import Utf8 from "crypto-js/enc-utf8";
 
-const el = document.createElement('div');
-el.innerText = 'Hello world!';
-document.body.appendChild(el);
+addHTML();
+
+encrbtn.addEventListener("click", function (e) {
+  const ciphertext = AES.encrypt(rawdata.value, password.value);
+  endata.value = ciphertext.toString();
+  display1.textContent = ciphertext.toString();
+});
+
+debtn.addEventListener("click", function (e) {
+  if (password.value != password2.value) {
+    alert("password must same as on encrypted");
+    return false;
+  }
+  const Normaltext = AES.decrypt(endata.value, password2.value);
+  display2.textContent = Normaltext.toString(Utf8);
+});
+
+
+function addHTML() {
+  const baseDiv = document.createElement("div");
+  baseDiv.id = "add_after_me";
+  document.body.appendChild(baseDiv);
+
+  document.getElementById("add_after_me").insertAdjacentHTML(
+    "afterend",
+    `<div>
+        <div>
+              Sensitive Data<input type="text" id="rawdata"/>
+              Password       <input type="text" id="password"/>
+              Encrypted data:: <span id="display1"></span><br/>
+              <button type="button" id="encrbtn">Encrypt</button>
+        </div>
+
+        <div>
+            Encrypted String<input type="text" id="endata"/>
+            Password       <input type="text" id="password2"/>
+            Decrypted data:: <span id="display2"></span><br/>
+            <button type="button" id="debtn">Decrypt</button>
+        </div>
+    <div/>`
+  );
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,26 @@
+const WebpackStatsViewerPlugin = require('webpack-stats-viewer-plugin').WebpackStatsViewerPlugin;
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require('path');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const path = require("path");
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = {
+  entry: "./src/index.js",
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'cryptojs-sandbox',
+      title: "cryptojs-sandbox",
     }),
-    new BundleAnalyzerPlugin(),
+    // new BundleAnalyzerPlugin(),
+    new WebpackStatsViewerPlugin(
+      {
+        stats: './dist/stats.json',
+        open: true,
+      }
+    ),
   ],
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "./dist"),
+    clean: true,
+  },
 };
